652c34e2124544136be39c1bcc2f0940ef37cc41
FlorealRISSO <floreal.risso@univ3-tlse.fr>
2023-01-21 13:02:50 +0000

    fix: std once style

--- src/util.h
+++ src/util.h
@@ -1,5 +1,10 @@
-#pragma once
+#ifndef _UTIL_H
+#define _UTIL_H
+
+//#warning "TODO: Add licence"
 
 #include <stdint.h>
 
 inline uint64_t substractAcc(const uint64_t l, const uint64_t r);
+
+#endif
