1a3157b0107b270137de33518e857110873bfbe3
FlorealRISSO <floreal.risso@univ3-tlse.fr>
2023-01-25 22:38:47 +0000

    cleanup

--- src/amd_rapl.c
+++ src/amd_rapl.c
@@ -25,6 +25,7 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "info_reader.h"
 #include "util.h"
@@ -269,30 +270,6 @@ void free_amd_rapl(_amd_rapl_t *rapl)
 
 unsigned int init_amd_rapl(char *none, void **ptr)
 {
-// 	FILE *fcpuinfo = fopen(cpuinfo, "r");
-// 	if (fcpuinfo == NULL) {
-// 		perror("fopen");
-// 		exit(1);
-// 	}
-//     Parser parser = {
-//         (GenericPointer) cpus,
-// 		0,
-//         MAX_CPUS,
-//         sizeof(cpu_sensor_t),
-//         keys,
-// 		NB_KEYS,
-// 		fcpuinfo
-//     };
-//     parse(&parser);
-// 	fclose(fcpuinfo);
-
-// #ifdef DEBUG
-// 	printf("nb_cpus: %d\n", parser.nb_stored);
-// 	for (unsigned int nb_cpu = 0; nb_cpu < parser.nb_stored; ++nb_cpu) {
-// 		debug_print_sensor(&cpus[nb_cpu]);
-// 	}
-// #endif
-
     UNUSED(none);
     _amd_rapl_t *rapl = (_amd_rapl_t *) calloc(1, sizeof(_amd_rapl_t));
 
@@ -320,7 +297,6 @@ unsigned int get_amd_rapl(uint64_t *results, void *ptr)
 {
     _amd_rapl_t *rapl = (_amd_rapl_t *) ptr;
     for (unsigned int i = 0; i < rapl->nb; i++) {
-        DEBUG_WARNING("check if unit is the same")
         results[i] = get_core_energy(&rapl->sensors[i]);
     }
     return rapl->nb;
@@ -347,179 +323,9 @@ void clean_amd_rapl(void *ptr)
 
 
 #ifdef __TESTING_AMD__
-#include "small_test.h"
-
-void test_raw_to_microjoule()
-{
-    printf("==== TEST raw_to_microjoule() ====\n");
-    uint64_t raw = 0;
-    uint64_t unit = 0;
-    uint64_t result = 0;
-    uint64_t expected = 0;
-
-    // Test 1:
-    // -- Setup
-    raw = 100;
-    unit = 0;
-    expected = 100000000;
-    // -- Run
-    result = raw_to_microjoule(raw, unit);
-    // -- Verification
-    TEST_UINT64_T(&result, &expected);
-
-    // TEST 2:
-    // -- Setup
-    raw = 200;
-    unit = 1;
-    expected = 100000000;
-    // -- Run
-    result = raw_to_microjoule(raw, unit);
-    // -- Verification
-    TEST_UINT64_T(&result, &expected);
-
-    // TEST 3:
-    // -- Setup
-    raw = 500;
-    unit = 2;
-    expected = 125000000;
-    // -- Run
-    result = raw_to_microjoule(raw, unit);
-    // -- Verification
-    TEST_UINT64_T(&result, &expected);
-
-    // TEST 4:
-    // -- Setup
-    raw = 1000;
-    unit = 3;
-    expected = 125000000;
-    // -- Run
-    result = raw_to_microjoule(raw, unit);
-    // -- Verification
-    TEST_UINT64_T(&result, &expected);
-
-    // TEST 5:
-    // -- Setup
-    raw = 10000;
-    unit = 4;
-    expected = 625000000;
-    // -- Run
-    result = raw_to_microjoule(raw, unit);
-    // -- Verification
-    TEST_UINT64_T(&result, &expected);
-}
-
-void test_get_name()
-{
-    printf("==== TEST get_name() ====\n");
-    size_t cpu_id = 0;
-    char *result = NULL;
-    char expected[100];
-
-    // TEST 1:
-    // -- Setup
-    cpu_id = 0;
-    strcpy(expected, "core0");
-    // -- Run
-    result = get_name(cpu_id);
-    // -- Verification
-    TEST_STR(result, expected);
-    free(result);
-
-    // TEST 2:
-    // -- Setup
-    cpu_id = 10000;
-    strcpy(expected, "core10000");
-    // -- Run
-    result = get_name(cpu_id);
-    // -- Verification
-    TEST_STR(result, expected);
-    free(result);
-}
-
-#define NONE 0
-#define DUMB_SENSOR(sensor, cpu_id, name)	\
-	do {									\
-		sensor = (cpu_sensor_t) {			\
-			cpu_id,							\
-			NONE,							\
-			name,							\
-			NONE,							\
-			NONE,							\
-			NONE, 							\
-			NONE							\
-		}; 									\
-	} while(0);
-
-#define DUMB_RAPL(rapl, sensors, nb)		\
-	do {									\
-		rapl = (_amd_rapl_t) {				\
-			sensors,						\
-			nb								\
-		};									\
-	} while(0);
-
-void test_label_amd_rapl()
-{
-    printf("==== TEST label_amd_rapl() ====\n");
-    cpu_sensor_t sensors[100];
-    _amd_rapl_t rapl;
-    char *results[100];
-    char expecteds[10][100];
-    uint64_t nb = 0;
-
-    // Test 1:
-    // -- Setup
-    nb = 1;
-    DUMB_SENSOR(sensors[0], 0, "core0");
-    DUMB_RAPL(rapl, sensors, nb);
-    strcpy(expecteds[0], "core0");
-    // -- Run
-    label_amd_rapl(results, (void *) &rapl);
-    // -- Verification
-    TEST_T_ARRAY(TEST_STR, nb, results, expecteds);
-
-    // Test 2:
-    // -- Setup
-    nb = 4;
-    DUMB_SENSOR(sensors[0], 0, "core0");
-    DUMB_SENSOR(sensors[1], 1, "core1");
-    DUMB_SENSOR(sensors[2], 2, "core2");
-    DUMB_SENSOR(sensors[3], 3, "core3");
-    DUMB_RAPL(rapl, sensors, nb);
-    strcpy(expecteds[0], "core0");
-    strcpy(expecteds[1], "core1");
-    strcpy(expecteds[2], "core2");
-    strcpy(expecteds[3], "core3");
-    // -- Run
-    label_amd_rapl(results, (void *) &rapl);
-    // -- Verification
-    TEST_T_ARRAY(TEST_STR, nb, results, expecteds);
-
-    // Test 3:
-    // -- Setup
-    nb = 4;
-    DUMB_SENSOR(sensors[0], 0, "core0");
-    DUMB_SENSOR(sensors[1], 3, "core3");
-    DUMB_SENSOR(sensors[2], 1, "core1");
-    DUMB_SENSOR(sensors[3], 2, "core2");
-    DUMB_RAPL(rapl, sensors, nb);
-    strcpy(expecteds[0], "core0");
-    strcpy(expecteds[1], "core3");
-    strcpy(expecteds[2], "core1");
-    strcpy(expecteds[3], "core2");
-    // -- Run
-    label_amd_rapl(results, (void *) &rapl);
-    // -- Verification
-    TEST_T_ARRAY(TEST_STR, nb, results, expecteds);
-}
-
-
+#ifdef DEBUG
 int main()
 {
-    test_raw_to_microjoule();
-    test_get_name();
-    test_label_amd_rapl();
-
     static const unsigned int time = 10;
     _amd_rapl_t *rapl = NULL;
     unsigned int nb_cpu = init_amd_rapl(NULL, (void **) &rapl);
@@ -548,5 +354,5 @@ int main()
     clean_amd_rapl(rapl);
     return 0;
 }
-
+#endif
 #endif
