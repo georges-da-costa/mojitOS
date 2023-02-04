823f32036b8e94e1543336d6a4045ac4d493f5ca
FlorealRISSO <floreal.risso@univ3-tlse.fr>
2023-02-04 15:18:26 +0000

    format

--- src/amd_rapl.c
+++ src/amd_rapl.c
@@ -25,8 +25,6 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <math.h>
 
 #include "info_reader.h"
 #include "util.h"
@@ -38,59 +36,66 @@ static const uint64_t amd_energy_mask = 0xFFFFFFFF;
 static const uint64_t amd_energy_unit_mask = 0x01F00;
 static const uint64_t msr_rapl_power_unit = 0xC0010299;
 static const uint64_t energy_core_msr = 0xC001029A;
-static const uint64_t energy_pkg_msr = 0xC001029B;
 
 // ------------------------------FILE_PATHS
 static const char *base_str = "/dev/cpu/%d/msr";
 
 struct cpu_sensor_t {
-    size_t cpu_id;
-    size_t package_id;
-    char *name;
+    unsigned int cpu_id;
+    unsigned int package_id;
+    unsigned int core_id;
 
+    char *name;
     int fd;
+
     unsigned int energy_units;
     uint64_t core_energy;
-    uint64_t pkg_energy;
 };
 typedef struct cpu_sensor_t cpu_sensor_t;
 
 struct _amd_rapl_t {
     cpu_sensor_t *sensors;
-    unsigned int nb;
+    unsigned int sensor_count;
 };
 typedef struct _amd_rapl_t _amd_rapl_t;
 
 // -----------------------------INFO_READER
 
 #ifdef __READ_CPUINFO__
-
 #warning "Must be modified before release"
 #define MAX_CPUS 64
-#define NB_KEYS 2
+#define NB_KEYS 3
 
 static const char *cpuinfo = "/proc/cpuinfo";
-static GenericPointer _size_t_allocator(char *s)
+
+static GenericPointer uint_allocator(char *s)
 {
-    size_t value = atoi(s);
+    unsigned int value = atoi(s);
     return (GenericPointer) value;
 }
 
 static void _set_cpu_id(GenericPointer storage, GenericPointer data)
 {
     cpu_sensor_t *cpu = (cpu_sensor_t *) storage;
-    cpu->cpu_id = (size_t) data;
+    cpu->cpu_id = (unsigned int) data;
 }
 
 static void _set_package_id(GenericPointer storage, GenericPointer data)
 {
     cpu_sensor_t *cpu = (cpu_sensor_t *) storage;
-    cpu->package_id = (size_t) data;
+    cpu->package_id = (unsigned int) data;
+}
+
+static void _set_core_id(GenericPointer storage, GenericPointer data)
+{
+    cpu_sensor_t *cpu = (cpu_sensor_t *) storage;
+    cpu->core_id = (unsigned int) data;
 }
 
 static KeyFinder keys[NB_KEYS] = {
-    {"processor", ": ", (CopyAllocator *) _size_t_allocator, (Setter *) _set_cpu_id},
-    {"physical id", ": ", (CopyAllocator *) _size_t_allocator, (Setter *) _set_package_id}
+    {"processor", ": ", uint_allocator, _set_cpu_id},
+    {"physical id", ": ", uint_allocator, _set_package_id},
+    {"core id", ": ", uint_allocator, _set_core_id}
 };
 #endif
 
@@ -119,13 +124,20 @@ uint64_t read_raw_core_energy(int fd)
     return energy & amd_energy_mask;
 }
 
+// -------------------------READ_PKG_ENERGY
+
+#ifdef __READ_PKG_ENERGY__
+// TODO: Verify if these functions are still useful (the package energy can be calculed)
+
+static const uint64_t energy_pkg_msr = 0xC001029B;
 uint64_t read_raw_pkg_energy(int fd)
 {
     uint64_t energy = read_msr(fd, energy_pkg_msr);
     return energy & amd_energy_mask;
 }
+#endif
 
-// ---------------------------------ENERGY
+// ----------------------------------ENERGY
 
 uint64_t raw_to_microjoule(uint64_t raw, unsigned int unit)
 {
@@ -147,20 +159,19 @@ uint64_t raw_to_joule(uint64_t raw, uint64_t unit)
 void debug_print_sensor(cpu_sensor_t *sensor)
 {
     //CASSERT(sizeof(cpu_sensor_t) == 56, amd_rapl_c);
-    printf("cpu_id : %ld, package_id : %ld, name : %s, fd: %d,  energy_units : %d, core_energy: %ld, pkg_energy: %ld\n",
+    printf("cpu_id : %d, package_id : %d, name : %s, fd: %d,  energy_units : %d, core_energy: %ld\n",
            sensor->cpu_id,
            sensor->package_id,
            sensor->name,
            sensor->fd,
            sensor->energy_units,
            sensor->core_energy,
-           sensor->pkg_energy
           );
 }
 
 void debug_print_amd_rapl(_amd_rapl_t *rapl)
 {
-    for (unsigned int i = 0; i < rapl->nb; i++) {
+    for (unsigned int i = 0; i < rapl->sensor_count; i++) {
         debug_print_sensor(&rapl->sensors[i]);
     }
 }
@@ -186,57 +197,46 @@ unsigned int get_nb_cpu()
     return n_cpu;
 }
 
-char *get_name(size_t cpu_id)
+char *get_name(unsigned int cpu_id)
 {
-    static const char *base_name = "core%ld";
+    static const char *base_name = "core%d";
     static const size_t max_lenght = 20;
     char *name = (char *)calloc(max_lenght, sizeof(char));
     snprintf(name, max_lenght, base_name, cpu_id);
     return name;
 }
 
-void init_cpu_sensor(cpu_sensor_t *sensor, unsigned int cpu_id)
+void update_cpu_sensor(cpu_sensor_t *sensor, uint64_t *energy_consumed)
 {
+    sensor->energy_units = read_unit(sensor->fd);
+    uint64_t raw_core_energy = read_raw_core_energy(sensor->fd);
+    uint64_t core_energy = raw_to_microjoule(raw_core_energy, sensor->energy_units);
+
+    *energy_consumed = modulo_substraction(core_energy, sensor->core_energy);
+    sensor->core_energy = core_energy;
+}
+
+unsigned int init_cpu_sensor(cpu_sensor_t *sensor, unsigned int cpu_id, unsigned char *cpus_map, unsigned int max_cpus)
+{
+    if (cpus_map[sensor->core_id * max_cpus + sensor->package_id] > 0) {
+        return 0;
+    }
+    cpus_map[sensor->core_id * max_cpus + sensor->package_id] += 1;
+
     static char filename[BUFFER_SIZE];
     sprintf(filename, base_str, cpu_id);
 
     int fd = open(filename, O_RDONLY);
     if (fd < 0) {
-        fprintf(stderr, "open(");
         fprintf(stderr, base_str, cpu_id);
-        perror(")");
+        perror(":open()");
         exit(127);
     }
-
-    uint64_t raw_core_energy = read_raw_core_energy(fd);
-    uint64_t raw_pkg_energy = read_raw_pkg_energy(fd);
 
     sensor->cpu_id = cpu_id;
     sensor->name = get_name(cpu_id);
     sensor->fd = fd;
-    sensor->energy_units = read_unit(fd);
-    sensor->core_energy = raw_to_microjoule(raw_core_energy, sensor->energy_units);
-    sensor->pkg_energy = raw_to_microjoule(raw_pkg_energy, sensor->energy_units);
-}
-
-uint64_t get_core_energy(cpu_sensor_t *sensor)
-{
-    uint64_t raw_core_energy = read_raw_core_energy(sensor->fd);
-    uint64_t core_energy = raw_to_microjoule(raw_core_energy, sensor->energy_units);
-
-    uint64_t energy_consumed = modulo_substraction(core_energy, sensor->core_energy);
-    sensor->core_energy = core_energy;
-    return energy_consumed;
-}
-
-uint64_t get_pkg_energy(cpu_sensor_t *sensor)
-{
-    uint64_t raw_pkg_energy = read_raw_pkg_energy(sensor->fd);
-    uint64_t pkg_energy = raw_to_microjoule(raw_pkg_energy, sensor->energy_units);
-
-    uint64_t energy_consumed = modulo_substraction(pkg_energy, sensor->pkg_energy);
-    sensor->pkg_energy = pkg_energy;
-    return energy_consumed;
+    return 1;
 }
 
 void clean_cpu_sensor(cpu_sensor_t *sensor)
@@ -258,41 +258,42 @@ unsigned int init_amd_rapl(char *none, void **ptr)
     UNUSED(none);
     _amd_rapl_t *rapl = (_amd_rapl_t *) calloc(1, sizeof(_amd_rapl_t));
 
-    unsigned int nb_cpu = get_nb_cpu();
-    if (nb_cpu == 0) {
-        fprintf(stderr, "open(");
-        fprintf(stderr, base_str,0);
-        perror(")");
+    unsigned int max_cpus = get_nb_cpu();
+    if (max_cpus == 0) {
+        fprintf(stderr, base_str, 0);
+        perror(":open()");
         exit(127);
     }
 
-    cpu_sensor_t *cpus = (cpu_sensor_t *) calloc(nb_cpu, sizeof(cpu_sensor_t));
-
-    rapl->nb = nb_cpu;
+    unsigned char *cpus_map = calloc(max_cpus * max_cpus, sizeof(unsigned char));
+    cpu_sensor_t *cpus = (cpu_sensor_t *) calloc(max_cpus, sizeof(cpu_sensor_t));
     rapl->sensors = cpus;
 
-    for (unsigned int i = 0; i < nb_cpu; i++) {
-        init_cpu_sensor(&rapl->sensors[i], i);
+    unsigned int nb_cpu = 0;
+    for (unsigned int i = 0; i < max_cpus; i++) {
+        nb_cpu += init_cpu_sensor(&rapl->sensors[nb_cpu], i, cpus_map, max_cpus);
     }
+    rapl->sensor_count = nb_cpu;
 
     *ptr = (void *) rapl;
-    return rapl->nb;
+    free(cpus_map);
+    return rapl->sensor_count;
 }
 
 
 unsigned int get_amd_rapl(uint64_t *results, void *ptr)
 {
     _amd_rapl_t *rapl = (_amd_rapl_t *) ptr;
-    for (unsigned int i = 0; i < rapl->nb; i++) {
-        results[i] = get_core_energy(&rapl->sensors[i]);
+    for (unsigned int i = 0; i < rapl->sensor_count; i++) {
+        update_cpu_sensor(&rapl->sensors[i], &results[i]);
     }
-    return rapl->nb;
+    return rapl->sensor_count;
 }
 
 void label_amd_rapl(char **labels, void *ptr)
 {
     _amd_rapl_t *rapl = (_amd_rapl_t *) ptr;
-    for (unsigned int i = 0; i < rapl->nb; i++) {
+    for (unsigned int i = 0; i < rapl->sensor_count; i++) {
         labels[i] = rapl->sensors[i].name;
     }
 }
@@ -301,9 +302,10 @@ void clean_amd_rapl(void *ptr)
 {
     _amd_rapl_t *rapl = (_amd_rapl_t *) ptr;
 
-    for (unsigned int i = 0; i < rapl->nb; ++i) {
+    for (unsigned int i = 0; i < rapl->sensor_count; ++i) {
         clean_cpu_sensor(&rapl->sensors[i]);
     }
     free(rapl->sensors);
     free(rapl);
 }
+
