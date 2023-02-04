3bb19b02e319a33b6c219c62b5a400553c3a0aa4
FlorealRISSO <floreal.risso@univ3-tlse.fr>
2023-02-04 15:18:03 +0000

    modification of test api

--- tests/main.c
+++ tests/main.c
@@ -2,7 +2,7 @@
 #include "amd_rapl.c"
 #include "info_reader.c"
 
-TFILE_ENTRY_POINT(main, {
+TMAIN({
     CALL_TFUNCTION(test_util);
     CALL_TFUNCTION(test_amd_rapl);
     CALL_TFUNCTION(test_info_reader);
