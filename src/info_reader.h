851cef1b09b4bf468aed0e11ca8c6cba31826067
FlorealRISSO <floreal.risso@univ3-tlse.fr>
2023-02-06 16:39:07 +0000

    fix: add ifndef

--- src/info_reader.h
+++ src/info_reader.h
@@ -1,3 +1,26 @@
+/*******************************************************
+ Copyright (C) 2023-2023 Georges Da Costa <georges.da-costa@irit.fr>
+
+    This file is part of Mojitos.
+
+    Mojitos is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Mojitos is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MojitO/S.  If not, see <https://www.gnu.org/licenses/>.
+
+*******************************************************/
+
+#ifndef _INFO_READER_H
+#define _INFO_READER_
+
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -215,4 +238,6 @@ static bool start_with(const char *prefix, const char *string)
         return  memcmp(prefix, string, prefix_len) == 0;
     }
 }
+
+#endif
 
