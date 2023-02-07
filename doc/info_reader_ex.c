189f9fc703b62d4ccfe7233bd0ee3271d696ceb0
FlorealRISSO <floreal.risso@univ3-tlse.fr>
2023-02-07 08:17:33 +0000

    cleanup

--- doc/info_reader_ex.c
+++ doc/info_reader_ex.c
@@ -23,8 +23,8 @@
 #include "./../src/info_reader.h"
 
 #define MAX_PROCS 64
-#define NB_KEYS 6
 
+// -----------------------------The storage
 typedef struct {
     unsigned int processor;
     char *vendor_id;
@@ -34,12 +34,16 @@ typedef struct {
     char *model_name;
 } Cpu;
 
+// --------------IMPLEMENTING THE INTERFACE
+
+// -- Define the behaviour if the attempted value is an int
 GenericPointer int_allocator(char *s)
 {
     unsigned int value = atoi(s);
     return (GenericPointer) value;
 }
 
+// -- Define the behaviour if the attempted value is a string
 GenericPointer string_allocator(char *s)
 {
     char *value = malloc(strlen(s) + 1);
@@ -47,31 +51,42 @@ GenericPointer string_allocator(char *s)
     return (GenericPointer) value;
 }
 
+// -- Define the processor setter
 void set_processor(GenericPointer storage, GenericPointer data)
 {
     Cpu *cpu = (Cpu *) storage;
     cpu->processor = data;
 }
+
+// -- Define the vendor_id setter
 void set_vendor_id(GenericPointer storage, GenericPointer data)
 {
     Cpu *cpu = (Cpu *) storage;
     cpu->vendor_id = (char *) data;
 }
+
+// -- Define the family setter
 void set_family(GenericPointer storage, GenericPointer data)
 {
     Cpu *cpu = (Cpu *) storage;
     cpu->family = data;
 }
+
+// -- Define the core_id setter
 void set_core_id(GenericPointer storage, GenericPointer data)
 {
     Cpu *cpu = (Cpu *) storage;
     cpu->core_id = data;
 }
+
+// -- Define the physical_id setter
 void set_physical_id(GenericPointer storage, GenericPointer data)
 {
     Cpu *cpu = (Cpu *) storage;
     cpu->physical_id = data;
 }
+
+// -- Define the model_name setter
 void set_model_name(GenericPointer storage, GenericPointer data)
 {
     Cpu *cpu = (Cpu *) storage;
@@ -81,28 +96,33 @@ void set_model_name(GenericPointer storage, GenericPointer data)
 int main()
 {
     Cpu cpus[MAX_PROCS];
+
+    // -- Define the setter, the allocator for each key / separator.
     KeyFinder keys[] = {
-        {"processor", ": ", (CopyAllocator *) int_allocator, (Setter *)set_processor},
-        {"vendor_id", ": ", (CopyAllocator *) string_allocator, (Setter *)set_vendor_id},
-        {"cpu family", ": ", (CopyAllocator *) int_allocator, (Setter *)set_family},
-        {"core id", ": ", (CopyAllocator *) int_allocator, (Setter *)set_core_id},
-        {"physical id", ": ", (CopyAllocator *) int_allocator,(Setter *)set_physical_id},
-        {
-            "model name", ": ", (CopyAllocator *) string_allocator,
-            (Setter *)set_model_name
-        }
+        {.key = "processor", .delimiter = ": ", .copy = (CopyAllocator *) int_allocator, .set = (Setter *)set_processor},
+        {.key = "vendor_id", .delimiter = ": ", .copy = (CopyAllocator *) string_allocator, .set = (Setter *)set_vendor_id},
+        {.key = "cpu family", .delimiter = ": ", .copy = (CopyAllocator *) int_allocator, .set = (Setter *)set_family},
+        {.key = "core id", .delimiter = ": ", .copy = (CopyAllocator *) int_allocator, .set = (Setter *)set_core_id},
+        {.key = "physical id", .delimiter = ": ", .copy = (CopyAllocator *) int_allocator, .set = (Setter *)set_physical_id},
+        {.key = "model name", .delimiter = ": ", .copy = (CopyAllocator *) string_allocator, .set = (Setter *)set_model_name}
     };
 
-    Parser parser = {.storage = (GenericPointer) cpus,
-                     .capacity = MAX_PROCS,
-                     .storage_struct_size = sizeof(Cpu),
-                     .keys = keys,
-                     .nb_keys = NB_KEYS,
-                     .file = fopen("/proc/cpuinfo", "r")
-                    };
+    size_t nb_keys = sizeof(keys)/sizeof(KeyFinder);
 
+    // -- Init the parser
+    Parser parser = {
+        .storage = (GenericPointer) cpus,
+        .capacity = MAX_PROCS,
+        .storage_struct_size = sizeof(Cpu),
+        .keys = keys,
+        .nb_keys = nb_keys,
+        .file = fopen("/proc/cpuinfo", "r")
+    };
+
+    // -- Parse the file
     parse(&parser);
 
+    // Print and free the results
     for (unsigned int i = 0; i < parser.nb_stored; ++i) {
         printf("========== PROC[%d] ==========\n", i);
         printf("Processor: %u\n", cpus[i].processor);
