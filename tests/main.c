32ff911bf1e940750eadf0e5080f374aa45921ef
FlorealRISSO <floreal.risso@univ-tlse3.fr>
2023-03-21 11:23:21 +0100

    add tests

--- tests/main.c
+++ tests/main.c
@@ -21,9 +21,11 @@
 #include "util.c"
 #include "amd_rapl.c"
 #include "info_reader.c"
+#include "memory.c"
 
 TMAIN({
     CALL_TFUNCTION(test_util);
     CALL_TFUNCTION(test_amd_rapl);
     CALL_TFUNCTION(test_info_reader);
+    CALL_TFUNCTION(test_memory);
 })
